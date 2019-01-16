06c6c74d7ce475d5e61d45c39687b2e008f35d4e
luxon <luxon@inbox.ru>
2019-01-16 13:28:23 +0300

    Update test_version.cpp

--- test_version.cpp
+++ test_version.cpp
@@ -2,16 +2,20 @@
 
 #include "lib.h"
 
-#include <boost/test/unit_test.hpp>
+// #include <boost/test/unit_test.hpp>
 
 #include <gtest/gtest.h>
 
-BOOST_AUTO_TEST_SUITE(test_version)
+/* BOOST_AUTO_TEST_SUITE(test_version)
 
 BOOST_AUTO_TEST_CASE(test_valid_version)
 {
     BOOST_CHECK(version() > 0);
 }
 
-
 BOOST_AUTO_TEST_SUITE_END()
+*/
+
+TEST(CustomOutputTest, PrintsMessage) {
+       EXPECT_TRUE( version() > 0 );
+}
