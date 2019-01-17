3db247daa1156f2f54424059a6b42bd20e121b93
luxon <luxon@inbox.ru>
2019-01-17 16:43:55 +0300

    Update test_version.cpp

--- test_version.cpp
+++ test_version.cpp
@@ -1,24 +1,14 @@
 #include "lib.h"
 #include "gtest/gtest.h"
 
-/*
-TEST(example, add)
-{
-    double res;
-    res = add_numbers(1.0, 2.0);
-    ASSERT_NEAR(res, 3.0, 1.0e-11);
-}
-
-
-TEST(MyTest, version) {
+TEST_F(MyTest, version) {
        MyTest t;
        EXPECT_LT( 0, t.version() );
 }
-*/   
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
- 
+
 
