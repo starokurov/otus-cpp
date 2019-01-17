55b8dc993bf2ee6792a1fe6ced5544c7838b5488
luxon <luxon@inbox.ru>
2019-01-17 15:59:24 +0300

    Update test_version.cpp

--- test_version.cpp
+++ test_version.cpp
@@ -1,39 +1,24 @@
-//#define BOOST_TEST_MODULE test_version
-
 #include "lib.h"
 #include "gtest/gtest.h"
 
 /*
-#include <boost/test/unit_test.hpp>
-
-#include "gtest/gtest.h"
-
-BOOST_AUTO_TEST_SUITE(test_version)
-
-BOOST_AUTO_TEST_CASE(test_valid_version)
+TEST(example, add)
 {
-    BOOST_CHECK(version() > 0);
+    double res;
+    res = add_numbers(1.0, 2.0);
+    ASSERT_NEAR(res, 3.0, 1.0e-11);
 }
 
-BOOST_AUTO_TEST_SUITE_END()
 
-*/
-/*
 TEST(MyTest, version) {
        MyTest t;
        EXPECT_LT( 0, t.version() );
 }
 */   
-    
-int main(int argc, char **argv)
-{
+
+int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
-
-    
-    for (int i = 1; i < argc; ++i) {
-        printf("arg %2d = %s\n", i, argv[i]);
-    }
-
     return RUN_ALL_TESTS();
 }
+ 
 
