a849f83e877df5500c38b1e22a346329990bf5ee
luxon <luxon@inbox.ru>
2019-01-17 10:44:46 +0300

    Update test_version.cpp

--- test_version.cpp
+++ test_version.cpp
@@ -16,6 +16,20 @@ BOOST_AUTO_TEST_CASE(test_valid_version)
 BOOST_AUTO_TEST_SUITE_END()
 
 
-/*TEST(CustomOutputTest, PrintsMessage) {
+/*
+TEST(CustomOutputTest, PrintsMessage) {
        EXPECT_TRUE( version() > 0 );
-}*/
+}
+    
+int _tmain(int argc, _TCHAR* argv[])
+{
+   /*The method is initializes the Google framework and must be called before RUN_ALL_TESTS */
+   ::testing::InitGoogleTest(&argc, argv);
+
+   /*RUN_ALL_TESTS automatically detects and runs all the tests defined using the TEST macro. 
+   It's must be called only once in the code because multiple calls lead to conflicts and, 
+   therefore, are not supported.
+   */ 
+   return RUN_ALL_TESTS();
+}
+*/
