8d2070bd7d0b4120a5cb6cf4768b78ef3f3337a4
s.koltsov <s.koltsov@visionlabs.ru>
2019-08-08 23:57:32 +0300

    revert for review.

---
+++ lib.cpp
@@ -0,0 +1,8 @@
+#include "lib.h"
+
+#include "version.h"
+
+int version()
+{
+    return PROJECT_VERSION_PATCH;
+}
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "lib.h"
+
+#include <iostream>
+
+int main (int, char **)
+{
+    std::cout << "Version: " << version() << std::endl;
+
+    return 0;
+}
