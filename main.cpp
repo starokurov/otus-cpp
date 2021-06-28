be34ad221fc9c6b30ef0c2a3ae8871b86b83f658
starokurov <starokurov@yandex.ru>
2021-06-28 18:51:55 +0300

    commit with bug

--- main.cpp
+++ main.cpp
@@ -4,6 +4,6 @@
 
 int main (int, char **) {
     std::cout << "Version: " << version() << std::endl;
-    std::cout << "Hello, world!" << std::endl;
+    std::cout << "Hello, world!" << std::endl
     return 0;
 }
